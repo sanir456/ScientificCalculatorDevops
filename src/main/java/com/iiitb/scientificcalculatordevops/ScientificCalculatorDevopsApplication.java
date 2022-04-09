f3aa85ca2163ad7aeb053aa35b6343b0e46f34be
sanir456 <saniranpariya456@gmail.com>
2022-04-09 15:12:36 +0530

    Initial commit

---
+++ src/main/java/com/iiitb/scientificcalculatordevops/ScientificCalculatorDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.iiitb.scientificcalculatordevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScientificCalculatorDevopsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScientificCalculatorDevopsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iiitb/scientificcalculatordevops/ScientificCalculatorDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iiitb.scientificcalculatordevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScientificCalculatorDevopsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
